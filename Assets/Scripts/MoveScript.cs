d2304d8214ad920b246a027defbe9f4bd550f3f2
thunderpeal <pealsgun@gmail.com>
2019-04-21 00:54:21 +0300

    MIN

---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    public float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float WS = Input.GetAxis("Vertical");
+        float AD = Input.GetAxis("Horizontal");
+
+        transform.position += transform.forward * WS * speed + transform.right * AD * speed;
+    }
+}
---
+++ Assets/Scripts/RotareCemeraScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotareCemeraScript : MonoBehaviour
+{
+    public bool IsRotareX = true, IsRotareY = true;
+    float angle;
+    float sense = 5f;
+    float angHor, angVer;
+    Quaternion originRotation;
+    void Start()
+    {
+        originRotation = transform.rotation;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        angHor += Input.GetAxis("Mouse X") * sense;
+        angVer += Input.GetAxis("Mouse Y") * sense;
+        Quaternion rotationY;
+        if (IsRotareY)
+        {
+            rotationY = Quaternion.AngleAxis(angHor, Vector3.up);
+        }
+        else
+        {
+            rotationY = Quaternion.AngleAxis(1, Vector3.up);
+        }
+        Quaternion rotationX;
+        if (IsRotareX)
+        {
+            rotationX = Quaternion.AngleAxis(-angVer, Vector3.right);
+        }
+        else
+        {
+            rotationX = Quaternion.AngleAxis(-1, Vector3.right);
+        }
+
+        angVer = Mathf.Clamp(angVer, -60, 60);
+
+
+
+        transform.rotation = originRotation * rotationY * rotationX;
+
+    }
+}
